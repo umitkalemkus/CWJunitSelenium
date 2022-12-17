65691613c71738f54b6ecefc1a6287e01cbe4452
umitkalemkus <umitkalemkus@yahoo.com>
2022-12-17 20:12:56 +0000

    17.12.2022

---
+++ src/test/java/Day04_LocatorPractise/Odev_1.java
@@ -0,0 +1,11 @@
+package Day04_LocatorPractise;
+
+public class Odev_1 {
+    //https://id.heroku.com/login sayfasına gidiniz.
+//Bir mail adresi giriniz.
+//Bir password giriniz.
+//Login butonuna tıklayınız.
+//"There was a problem with your login." text görünür ise "Kayıt Yapılamadı" yazdırınız.
+//Eğer yazı görünür değilse "Kayıt Yapıldı" yazdırınız.
+//Tüm sayfaları kapatınız.
+}
---
+++ src/test/java/Day04_LocatorPractise/Odev_2.java
@@ -0,0 +1,11 @@
+package Day04_LocatorPractise;
+
+public class Odev_2 {
+    //Navigate to https://testpages.herokuapp.com/styled/index.html
+// Click on Calculate under Micro Apps.
+// Type any number in the first input.
+// Type any number in the second input.
+// Click on Calculate.
+// Get the result.
+// Print the result.
+}
