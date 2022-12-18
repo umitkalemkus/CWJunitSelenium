2d804d20abb2160f8c6f56a125a36f7b425cd6c2
umitkalemkus <umitkalemkus@yahoo.com>
2022-12-18 00:17:34 +0000

    18.12.2022

--- src/test/java/Day04_LocatorPractise/Odev_1.java
+++ src/test/java/Day04_LocatorPractise/Odev_1.java
@@ -1,5 +1,16 @@
 package Day04_LocatorPractise;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
 public class Odev_1 {
     //https://id.heroku.com/login sayfasına gidiniz.
 //Bir mail adresi giriniz.
@@ -8,4 +19,53 @@ public class Odev_1 {
 //"There was a problem with your login." text görünür ise "Kayıt Yapılamadı" yazdırınız.
 //Eğer yazı görünür değilse "Kayıt Yapıldı" yazdırınız.
 //Tüm sayfaları kapatınız.
+    WebDriver driver;
+
+    @Before
+
+    public void setup() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().window().maximize();
+
+
+    }
+
+    @After
+    public void tearDown() {
+
+       // driver.quit();
+
+
+    }
+
+    @Test
+    public void test() {
+
+        driver.get("https://id.heroku.com/login");
+
+        WebElement email = driver.findElement(By.id("email"));
+        email.click();
+        email.sendKeys("clarusway1234@yahoo.com");
+
+
+        WebElement password = driver.findElement(By.id("password"));
+        password.click();
+        password.sendKeys("123456");
+
+        WebElement login = driver.findElement(By.name("commit"));
+        login.click();
+
+        WebElement alert = driver.findElement(By.xpath("//*[@class='alert alert-danger']"));
+        String LoginProblem = alert.getText();
+        System.out.println(LoginProblem);
+         if (alert.getText().equals(LoginProblem)){
+             System.out.println("Kayit yapilamadi");
+         }else
+             System.out.println("Kayit yapildi");
+
+
+
+    }
 }
--- src/test/java/Day04_LocatorPractise/Odev_2.java
+++ src/test/java/Day04_LocatorPractise/Odev_2.java
@@ -1,5 +1,17 @@
 package Day04_LocatorPractise;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
 public class Odev_2 {
     //Navigate to https://testpages.herokuapp.com/styled/index.html
 // Click on Calculate under Micro Apps.
@@ -8,4 +20,69 @@ public class Odev_2 {
 // Click on Calculate.
 // Get the result.
 // Print the result.
+
+    WebDriver driver;
+    @Before
+    public  void setup(){
+        WebDriverManager.chromedriver().setup();
+        driver =new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+
+        driver.manage().window().maximize();
+
+
+
+
+
+
+    }
+
+    @After
+    public void tearDown(){
+
+        driver.quit();
+
+
+
+
+
+    }
+    @Test
+    public void test(){
+
+        driver.get("https://testpages.herokuapp.com/styled/index.html");
+        WebElement Calculate = driver.findElement(By.id("calculatetest"));
+        Calculate.click();
+
+        WebElement input1 = driver.findElement(By.name("number1"));
+
+        input1.click();
+        input1.sendKeys("10");
+
+        WebElement input2 = driver.findElement(By.name("number2"));
+        input2.click();
+        input2.sendKeys("100");
+
+
+        WebElement CalculateButton = driver.findElement(By.id("calculate"));
+        CalculateButton.click();
+
+
+
+        WebElement Result  = driver.findElement(By.id("answer"));
+        String Answer = Result.getText();
+        System.out.println(Answer);
+
+
+
+
+
+
+    }
+
+
+
+
+
 }
