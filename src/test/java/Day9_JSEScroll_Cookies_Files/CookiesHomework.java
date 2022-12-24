2260ae5689f719156b3e8069641b680b9b4fa642
umitkalemkus <umitkalemkus@yahoo.com>
2022-12-24 00:25:14 +0000

    24.12.2022

---
+++ src/test/java/Day9_JSEScroll_Cookies_Files/CookiesHomework.java
@@ -0,0 +1,46 @@
+package Day9_JSEScroll_Cookies_Files;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.Cookie;
+import utilities.BaseTest;
+
+import java.util.Set;
+
+public class CookiesHomework extends BaseTest {
+
+   /* Go to URL: http://facebook.com
+    getCookies,
+    addCookie,
+    deleteCookieNamed,
+    deleteAllCookies.*/
+
+    @Test
+    public void Cookies() {
+
+        //getCookies,
+        driver.get("http://facebook.com");
+        Set<Cookie> allCookies = driver.manage().getCookies();
+        int numberofcookies = driver.manage().getCookies().size();
+        System.out.println("numberofcookies = " + numberofcookies);
+
+        //addCookie,
+        System.out.println("add cookie");
+        Cookie cookies1 = new Cookie("Cheese", "Cecil");
+        driver.manage().addCookie(cookies1);
+        Assert.assertEquals(cookies1.getValue(),driver.manage().getCookieNamed(cookies1.getName()).getValue());
+        System.out.println(driver.manage().getCookieNamed(cookies1.getName()).getValue());
+
+        //deleteCookieNamed,
+
+
+
+
+
+
+
+
+
+
+    }
+}
