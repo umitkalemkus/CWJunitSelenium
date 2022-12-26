810b24ac6837ea07050f58901134defba126711e
umitkalemkus <umitkalemkus@yahoo.com>
2022-12-26 23:55:19 +0000

    26.12.2022

---
+++ src/test/java/Day9_JSEScroll_Cookies_Files/C08_FileExist.java
@@ -0,0 +1,38 @@
+package Day9_JSEScroll_Cookies_Files;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.File;
+
+public class C08_FileExist {
+
+
+
+
+    @Test
+    public void fileExist() {
+        //C:\\Users\\umitkalemkus\\Desktop\\fileExist.png
+        System.out.println(System.getProperty("user.home"));  // Home yolunu yazdirdik
+        String  homePath =System.getProperty("user.home");   // Home yolunu atadik
+
+        String filePath = "\\Desktop\\fileExist.png";     // Dosyanin kalan yolunu atadik
+
+        String fullPath = homePath + filePath;     // home ve dosya yollarini birlestirip full pathi elde ettik.
+
+        File image = new File(fullPath);        // Path imizi file a cevirdik.
+
+        Assert.assertTrue(image.exists());    // exist metoduyla varolup olmadigini kontrol ettik.
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
