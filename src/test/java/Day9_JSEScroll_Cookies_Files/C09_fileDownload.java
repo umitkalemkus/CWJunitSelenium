42933d1901974532e589df977867721286972f33
umitkalemkus <umitkalemkus@yahoo.com>
2022-12-27 00:36:57 +0000

    27.12.2022

--- src/test/java/Day9_JSEScroll_Cookies_Files/C09_fileDownload.java
+++ src/test/java/Day9_JSEScroll_Cookies_Files/C09_fileDownload.java
@@ -1,10 +1,14 @@
 package Day9_JSEScroll_Cookies_Files;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import utilities.BaseTest;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class C09_fileDownload extends BaseTest {
 
@@ -15,7 +19,7 @@ public class C09_fileDownload extends BaseTest {
 */
 
     @Test
-    public void name() {
+    public void name() throws InterruptedException {
         driver.get("https://opensource-demo.orangehrmlive.com/");
         WebElement userName = driver.findElement(By.name("username"));
         userName.sendKeys("Admin");
@@ -24,6 +28,25 @@ public class C09_fileDownload extends BaseTest {
 
         WebElement loginButton = driver.findElement(By.xpath("//button[@type='submit']"));
         loginButton.click();
+
+        WebElement PIM = driver.findElement(By.xpath("(//li[@class ='oxd-main-menu-item-wrapper'])[2]"));
+        PIM.click();
+
+        WebElement Configurations = driver.findElement(By.xpath("(//span[@class='oxd-topbar-body-nav-tab-item'])[1]"));
+        Configurations.click();
+
+        WebElement DataImport = driver.findElement(By.linkText("Data Import"));
+        DataImport.click();
+
+        WebElement Download =driver.findElement(By.className("download-link"));
+        Download.click();
+        Thread.sleep(5000);
+
+        String downloadPath = "C:\\Users\\umitkalemkus\\Downloads\\importData.csv";
+
+       Assert.assertTrue("Indirilen dosya bulunamadi",Files.exists(Paths.get(downloadPath)));
+
+
 
 
 
