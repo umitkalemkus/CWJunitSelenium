c0d539cf8078bfe8a59131a08718d3199655ee7e
umitkalemkus <umitkalemkus@yahoo.com>
2022-12-28 14:10:04 +0000

    28.12.2022

--- src/test/java/Day12_Screenshots/C02_Screenshots.java
+++ src/test/java/Day12_Screenshots/C02_Screenshots.java
@@ -1,24 +1,51 @@
 package Day12_Screenshots;
 
+import org.apache.commons.io.FileUtils;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.io.FileHandler;
 import utilities.BaseTest;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class C02_Screenshots extends BaseTest {
 
 
     @Test
-    public void getPageScreenshot() {
+    public void getPageScreenshot() throws IOException {
+        driver.get("https://www.n11.com/");
 
+        //Screenshot umuzu File class indan bir objeye aliyoruz
+        File kaynak = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
 
-
+        // Aldigimiz screenshot istedigimiz path e kaydediyoruz
+        FileHandler.copy(kaynak, new File("test-output/Screenshots/n11PageScreenshot.png"));
     }
 
+
     @Test
-    public void getElementScreenshot() {
+    public void getElementScreenshot() throws IOException {
 
+        driver.get("https://opensource-demo.orangehrmlive.com/");
 
+        //Screenshot u alinacak elementimizi locate ettik
+        WebElement logo = driver.findElement(By.className("orangehrm-login-branding"));
 
+        // Element uzerinden screenshot umuzu aldik
+        File logoScreenshot = logo.getScreenshotAs(OutputType.FILE);
 
+        // Ozel isimler olabilmesi adina date imizi dosya isimlerimizin yanina ekledik
+        String date = new SimpleDateFormat("yyyy-MM-dd_hh-mm-ss").format(new Date());
 
+        String path = "test-output\\Screenshots\\logoScreenshot_" + date + ".png";
+
+        // Dosyamizi kaydettik
+        FileUtils.copyFile(logoScreenshot, new File(path));
     }
 }
---
+++ src/test/java/Day12_Screenshots/C03_WebTables.java
@@ -0,0 +1,30 @@
+package Day12_Screenshots;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import utilities.BaseTest;
+
+
+
+    public class C03_WebTables extends BaseTest {
+
+
+        @Test
+        public void webTableExample() {
+
+            driver.get("http://demo.guru99.com/test/web-table-element.php");
+
+            //Tablomuzu locate ettik
+            WebElement table = driver.findElement(By.className("dataTable")).findElement(By.tagName("tbody"));
+
+            // 3. satir locate edildi
+            WebElement row3 = table.findElement(By.xpath(".//tr[3]"));
+
+            //3. satir sutun hucre locate edildi
+
+            WebElement row3cell3 = row3.findElement(By.xpath())
+
+            System.out.println(row3.getText());
+        }
+    }
