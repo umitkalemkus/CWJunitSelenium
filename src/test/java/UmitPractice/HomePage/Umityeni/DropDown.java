ac3ff44cd41ce0dc44c9e599a2e411932a8f0ffe
umitkalemkus <umitkalemkus@yahoo.com>
2023-07-16 00:18:19 +0100

    done

--- src/test/java/UmitPractice/HomePage/Umityeni/DropDown.java
+++ src/test/java/UmitPractice/HomePage/Umityeni/DropDown.java
@@ -4,9 +4,11 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 
 import java.time.Duration;
 
@@ -40,7 +42,13 @@ public class DropDown {
 
 
     @Test
-    public void dropDown() {
+    public void dropDown() throws InterruptedException {
+
+        driver.get("https://demo.guru99.com/test/newtours/register.php");
+        Select select = new Select(driver.findElement(By.xpath("//select[@name='country']")));
+        //select.selectByIndex(2);
+       // select.selectByValue("8");
+        select.selectByVisibleText("NEPAL");
 
 
 
