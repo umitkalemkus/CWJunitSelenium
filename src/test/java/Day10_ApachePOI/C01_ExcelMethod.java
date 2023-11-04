05d339f1809fc73971c448219df8ece8a23d0a91
umitkalemkus <umitkalemkus@yahoo.com>
2023-11-04 23:43:04 +0000

    27.10.10

--- src/test/java/Day10_ApachePOI/C01_ExcelMethod.java
+++ src/test/java/Day10_ApachePOI/C01_ExcelMethod.java
@@ -12,6 +12,7 @@ public class C01_ExcelMethod {
 
 
 
+
           String path ="src\\test\\java\\resources\\excelfile.xlsx";
           //Dosyasimi actik
           FileInputStream fis = new FileInputStream(path);
